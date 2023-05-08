7571ed60d9b4791bb64bc691b3fe76c98cf6d22a
Hung Tran (Web) <hung.tran@bbv.vn>
2023-05-08 15:58:31 +0700

    chore: 2nd test with TestBed

---
+++ src/app/welcome/welcome.component.spec.ts
@@ -0,0 +1,51 @@
+import { UserService } from './user.service';
+import { WelcomeComponent } from './welcome.component';
+import { TestBed } from '@angular/core/testing';
+
+class MockUserService {
+  isLoggedIn = true;
+  user = { name: 'Test User' };
+}
+
+describe('WelcomeComponent (class only)', () => {
+  let comp: WelcomeComponent;
+  let userService: UserService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      // provide the component-under-test and dependent service
+      providers: [
+        WelcomeComponent,
+        { provide: UserService, useClass: MockUserService },
+      ],
+    });
+    // inject both the component and the dependent service.
+    comp = TestBed.inject(WelcomeComponent);
+    userService = TestBed.inject(UserService);
+  });
+
+  it('should not have welcome message after construction', () => {
+    // Assert
+    expect(comp.welcome).toBe('');
+  });
+
+  it('should welcome logged in user after Angular calls ngOnInit', () => {
+    // Act
+    comp.ngOnInit();
+
+    // Assert
+    expect(comp.welcome).toContain(userService.user.name);
+  });
+
+  it('should ask user to log in if not logged in after ngOnInit', () => {
+    // Arrange
+    userService.isLoggedIn = false;
+
+    // Act
+    comp.ngOnInit();
+
+    // Assert
+    expect(comp.welcome).not.toContain(userService.user.name);
+    expect(comp.welcome).toContain('log in');
+  });
+});
