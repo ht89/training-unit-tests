9089beda772dca463038b2179b57ef24f3038f40
Hung Tran (Web) <hung.tran@bbv.vn>
2023-06-13 10:55:52 +0700

    chore: refactor twain service

--- src/app/twain/twain.service.ts
+++ src/app/twain/twain.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 import { Observable, of, throwError, Observer } from 'rxjs';
-import { concatWith, map, retry, switchMap, take } from 'rxjs/operators';
+import { concatWith, map, retryWhen, switchMap, take } from 'rxjs/operators';
 
 import { Quote } from './quote';
 
@@ -18,33 +18,34 @@ export class TwainService {
       observer.next(this.nextId++)
     ).pipe(
       // tap((id: number) => console.log(id)),
-      // tap((id: number) => { throw new Error('Simulated server error'); }),
+      // tap((id: number) => {
+      //   throw new Error('Simulated server error');
+      // }),
 
       switchMap((id: number) => this.http.get<Quote>(`api/quotes/${id}`)),
       // tap((q : Quote) => console.log(q)),
       map((q: Quote) => q.quote),
 
       // `errors` is observable of http.get errors
-      retry({
-        delay: (errors) =>
-          errors.pipe(
-            switchMap((error: HttpErrorResponse) => {
-              if (error.status === 404) {
-                // Queried for quote that doesn't exist.
-                this.nextId = 1; // retry with quote id:1
-                return of(null); // signal OK to retry
-              }
-              // Some other HTTP error.
-              console.error(error);
-              return throwError(
-                () => new Error('Cannot get Twain quotes from the server')
-              );
-            }),
-            take(2),
-            // If a second retry value, then didn't find id:1 and triggers the following error
-            concatWith(throwError(() => new Error('There are no Twain quotes'))) // didn't find id:1
-          ),
-      })
+      retryWhen((errors) =>
+        errors.pipe(
+          switchMap((error: HttpErrorResponse) => {
+            if (error.status === 404) {
+              // Queried for quote that doesn't exist.
+              this.nextId = 1; // retry with quote id:1
+              return of(null); // signal OK to retry
+            }
+            // Some other HTTP error.
+            console.error(error);
+            return throwError(
+              () => new Error('Cannot get Twain quotes from the server')
+            );
+          }),
+          take(2),
+          // If a second retry value, then didn't find id:1 and triggers the following error
+          concatWith(throwError(() => new Error('There are no Twain quotes'))) // didn't find id:1
+        )
+      )
     );
   }
 }
